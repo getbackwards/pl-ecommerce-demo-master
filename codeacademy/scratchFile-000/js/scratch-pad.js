cff604042613a113dffa3f5bf6d93e4727759d35
tony gray <getbackwards@tonys-MacBook-Pro.local>
2018-09-30 21:31:31 +1300

    bunch of stuff

--- codeacademy/scratchFile-000/js/scratch-pad.js
+++ codeacademy/scratchFile-000/js/scratch-pad.js
@@ -1,11 +1,25 @@
 const story = "Last weekend, I took literally the most beautiful bike ride of my life.";
 // console.log('Test console log.');
-const regex = /,/gi;
+
+const regex = /[.,\s]/g;
+// const regex = /[\s]/g;
+
 //console.log(regex);
 
-const string_to_array = function (str) {
-    return str.trim().split(" ");
-};
+// const string_to_array = function (str) {
+//     return str.trim().split(" ");
+// };
+
 // console.log(string_to_array.replace(regex, 'ferret,'));
-console.log(story.replace(regex, ''));
+console.log(story.split(regex));
+// console.log(story.split());
+// console.log(story);
+
+// console.log(story.replace(regex, ''));
+
 // console.log(string_to_array("Last weekend, I took literally the most beautiful bike ride of my life wow."));
+
+let unnecessaryWords = ['extremely', 'literally', 'actually' ];
+
+// console.log(unnecessaryWords.join(" "));
+console.log(unnecessaryWords);

f7b14f726bdf0ad784c23385416c8a2efc9e6fa9
Your Name <getbackwards@gmail.com>
2018-10-14 16:18:42 +1300

    sort by teeth

---
+++ codeacademy/scratchFile-000/js/sortSpeciesByTeeth.js
@@ -0,0 +1,16 @@
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+
+// Write your code here:
+const sortSpeciesByTeeth = (input) => {
+    return input.sort((a, b) => a.numTeeth - b.numTeeth);
+};
+
+// Feel free to comment out the code below when you're ready to test your function!
+
+console.log(sortSpeciesByTeeth(speciesArray));
+
+// Should print [ { speciesName: 'human', numTeeth: 32 },
+//  { speciesName: 'dog', numTeeth: 42 },
+//  { speciesName: 'shark', numTeeth: 50 },
+//  { speciesName: 'alligator', numTeeth: 80 } ]
+

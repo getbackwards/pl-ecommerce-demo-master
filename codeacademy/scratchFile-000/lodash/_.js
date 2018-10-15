7e0edf0f5ba457b1c22990ee83f1acc2eb6011e6
Tony Gray <getbackwards@gmail.com>
2018-10-16 10:49:57 +1300

    working on proj

--- codeacademy/scratchFile-000/lodash/_.js
+++ codeacademy/scratchFile-000/lodash/_.js
@@ -1,10 +1,17 @@
 const _ = {
+    clamp (number, lower, upper) {
+        let lowerBound = Math.max(number, lower);
+        let upperBound = Math.min(number, upper);
+        // console.log('This is the numer ' + number);
 
-    const clamp = (number, lower, upper) => {
+        let output = [`The number is ${number}`, `The lowerBound is ${lowerBound}`, `The upperBound is ${upperBound}`];
 
+        return output;
     }
-
 };
+console.log(_.clamp(17, 5, 15));
+console.log(_.clamp(3, 5, 15));
+console.log(_.clamp(7, 5, 15));
 
 // Do not write or modify code below this line.
 module.exports = _;
